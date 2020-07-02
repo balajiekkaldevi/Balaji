0afbc9e67dcfc4858a24c2dfa7bc72eaf2d54da3
balajiekkaldevi <57043303+balajiekkaldevi@users.noreply.github.com>
2020-07-02 18:52:53 +0530

    Create day_13.py

---
+++ day_13.py
@@ -0,0 +1,8 @@
+ 
+class MyBook(Book):
+    def __init__(self, title, author, price):
+        Book.__init__(self, title, author)
+        self.price = price
+        
+    def display(self):
+        print("Title: %s\nAuthor: %s\nPrice: %s" %(title, author, price))
